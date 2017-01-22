6b13884d0b9c23d14f2e7f110dda56a103b223f9
Chen Li <yichen.li0830@gmail.com>
2017-01-21 17:45:02 -0800

    Reuse components

--- index.ios.js
+++ index.ios.js
@@ -2,4 +2,4 @@ import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './src/app';
 
-AppRegistry.registerComponent('RN_tech_stack', App);
+AppRegistry.registerComponent('RN_tech_stack', ()=>App);
---
+++ src/components/common/index.js
@@ -0,0 +1,16 @@
+// import Button from './Button';
+// import Card from './Card';
+// import CardSection from './CardSection';
+// import Header from './header';
+//
+// export {Button, Card, CardSection, Header};
+
+export * from './Button';
+export * from './CardSection';
+export * from './Card';
+export * from './Header';
+export * from './Input';
+export * from './Spinner';
+
+
+
